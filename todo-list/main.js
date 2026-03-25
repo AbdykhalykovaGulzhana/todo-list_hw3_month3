f827c3e82b58fe3bb5bb795bce9f62840be4990a
AbdykhalykovaGulzhana <abdykhalykovag@icloud.com>
2026-03-25 11:33:49 +0600

    add details

--- todo-list/main.js
+++ todo-list/main.js
@@ -14,6 +14,57 @@ const inputCreateElement = document.querySelector("#todo-input");
 const buttonCreateElement = document.querySelector("#todo-form button");
 const todoListElement = document.querySelector("#todo-list");
 
+let tasks = [];
+
+const renderTasks = () => {
+    todoListElement.innerHTML = "";
+
+
+    tasks.forEach((task, index) => {
+        const taskElement = document.createElement("li");
+        todoListElement.append(taskElement);
+        taskElement.classList.add("task")
+        // taskElement.classList.add("completed");
+
+        const checkboxElement = document.createElement("input");
+        taskElement.append(checkboxElement);
+        checkboxElement.setAttribute("type", "checkbox");
+
+        const nameElement = document.createElement("span");
+        taskElement.append(nameElement);
+        nameElement.innerText = task.name;
+
+        checkboxElement.checked = task.isCompleted;
+
+        checkboxElement.addEventListener("change", (event) => {
+
+            task.isCompleted = event.target.checked;
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+
+            if (task.isCompleted) {
+                alert("Task: '" + nameElement.innerText + "' is successfully completed.")
+            }
+            taskElement.classList.toggle("completed");
+            console.log(tasks);
+        })
+
+        const deleteElement = document.createElement("button");
+        deleteElement.innerText = "x";
+        taskElement.append(deleteElement);
+        deleteElement.addEventListener("click", () => {
+            tasks.splice(index, 1);
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            renderTasks();
+        });
+    });
+    console.log(tasks)
+};
+const savedTasks = localStorage.getItem("tasks");
+if (savedTasks) {
+    tasks = JSON.parse(savedTasks);
+    renderTasks();
+}
+
 const createTask = (event) => {
     event.preventDefault();
 
@@ -21,30 +72,31 @@ const createTask = (event) => {
         return alert("Enter the name of task: ")
     }
 
-    const taskElement = document.createElement("li");
-    todoListElement.append(taskElement);
-    taskElement.classList.add("task")
-    // taskElement.classList.add("completed");
+    tasks.push({
+    name: inputCreateElement.value.trim(),
+    isCompleted: false,
 
-    const checkboxElement = document.createElement("input");
-    taskElement.append(checkboxElement);
-    checkboxElement.setAttribute("type", "checkbox");
-    checkboxElement.addEventListener("click", () => {
-       taskElement.classList.add("completed");
-       alert("Task" + " " + nameElement.innerText + " is successfully completed.")
-    });
-
-    let nameElement = document.createElement("span");
-    taskElement.append(nameElement);
-    nameElement.innerText = inputCreateElement.value.trim();
+});
+    localStorage.setItem("tasks", JSON.stringify(tasks));
     inputCreateElement.value = "";
+    renderTasks();
 
-
-    const deleteElement = document.createElement("button");
-    deleteElement.innerText = "x";
-    taskElement.append(deleteElement);
-    deleteElement.addEventListener("click", () => {
-        taskElement.remove();
-    });
 }
+const themeBtn = document.getElementById("theme-btn");
+
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+}
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    const isDark = document.body.classList.contains("dark");
+    if (isDark) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+});
 buttonCreateElement.addEventListener("click", createTask);
